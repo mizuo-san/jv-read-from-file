fc15aee8dc66a93bc71ff8c6d1e9cc49f0583f13
dkks112313 <ovcharenkoden05@gmail.com>
2025-01-08 21:04:12 +0200

    hi, fix stuff

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,50 @@
 package core.basesyntax;
 
+import java.io.*;
+import java.util.Arrays;
+import java.util.List;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        StringBuilder wText = new StringBuilder();
+        List<Character> ar = List.of('.', ',', '!', '?');
+
+        try(BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            if(checkFileEmpty(fileName)) {
+                return new String[0];
+            }
+
+            String read = br.readLine();
+
+            while (read != null) {
+                String[] words = read.split(" ");
+                for (String word : words) {
+                    word = word.toLowerCase();
+
+                    if(ar.contains(word.charAt(word.length() - 1))) {
+                        word = word.substring(0, word.length() - 1);
+                    }
+
+                    if(word.charAt(0) == 'w') {
+                        wText.append(word.toLowerCase()).append(" ");
+                    }
+                }
+
+                read = br.readLine();
+            }
+        } catch (IOException e) {}
+
+        if(wText.isEmpty()) {
+            return new String[0];
+        }
+
+        String[] words = wText.toString().split(" ");
+        Arrays.sort(words);
+        return words;
+    }
+
+    private boolean checkFileEmpty(String fileName) {
+        File fw = new File(fileName);
+        return fw.length() == 0;
     }
 }
